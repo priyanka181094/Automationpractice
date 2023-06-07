345cb8be3e61d7a860e09c933afbc17aaa1b5257
priyanka181094 <priyankaghosh181094@gmail.com>
2023-06-07 12:32:34 +0530

    second commit

--- src/test/java/com/pavantesting/Development.java
+++ src/test/java/com/pavantesting/Development.java
@@ -5,6 +5,7 @@ public class Development {
 	public static void main(String[] args) {
 		System.out.println("Bye");
 		System.out.println("Good night");
+		System.out.println("hello");
 		// TODO Auto-generated method stub
 
 	}
