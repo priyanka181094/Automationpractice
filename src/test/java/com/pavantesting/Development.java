c31c8b916f2aba8402d811c2adc7829a7b0d3724
priyanka181094 <priyankaghosh181094@gmail.com>
2023-06-07 12:16:30 +0530

    firstcommit

---
+++ src/test/java/com/pavantesting/Development.java
@@ -0,0 +1,12 @@
+package com.pavantesting;
+
+public class Development {
+
+	public static void main(String[] args) {
+		System.out.println("Bye");
+		System.out.println("Good night");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/pavantesting/Testing.java
@@ -0,0 +1,12 @@
+package com.pavantesting;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("Good morning");
+		System.out.println("How are you");
+		// TODO Auto-generated method stub
+
+	}
+
+}
